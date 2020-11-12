b590b2f9ba1ea0fb3b7eb446940e76e7d5fac019
AndreaBelen22 <belenorregoa@gmail.com>
2020-11-12 18:20:11 +0100

    added

---
+++ testing.js
@@ -0,0 +1,6 @@
+const toTitleCase = require(`to-title-case`);
+console.log(toTitleCase);
+
+const lower = ('the lord of the rings: the return of the king');
+const tittle = toTitleCase(lower);
+console.log(tittle);
